3ff0ce4e9c593489d4a1444009c2619fd56238c8
Rodrigo Mariano <rodmariano13@gmail.com>
2019-03-12 16:34:25 -0300

    Updated the .ol3 class to .ol class on OL 3

--- js/ol3-sidebar.js
+++ js/ol3-sidebar.js
@@ -12,34 +12,34 @@ ol.control.Sidebar = function (settings) {
         target: this._options.target
     });
 
-    // Attach .ol3-sb-sidebar-left/right class
-    this.element.classList.add('ol3-sb-sidebar-' + this._options.position);
+    // Attach .ol-sb-sidebar-left/right class
+    this.element.classList.add('ol-sb-sidebar-' + this._options.position);
 
-    // Find ol3-sb-sidebar > div.ol3-sb-sidebar-content
+    // Find ol-sb-sidebar > div.ol-sb-sidebar-content
     for (i = this.element.children.length - 1; i >= 0; i--) {
         child = this.element.children[i];
         if (child.tagName === 'DIV' &&
-                child.classList.contains('ol3-sb-sidebar-content')) {
+                child.classList.contains('ol-sb-sidebar-content')) {
             this._container = child;
         }
     }
 
-    // Find ol3-sb-sidebar ul.ol3-sb-sidebar-tabs > li, ol3-sb-sidebar .ol3-sb-sidebar-tabs > ul > li
-    this._tabitems = this.element.querySelectorAll('ul.ol3-sb-sidebar-tabs > li, .ol3-sb-sidebar-tabs > ul > li');
+    // Find ol-sb-sidebar ul.ol-sb-sidebar-tabs > li, ol-sb-sidebar .ol-sb-sidebar-tabs > ul > li
+    this._tabitems = this.element.querySelectorAll('ul.ol-sb-sidebar-tabs > li, .ol-sb-sidebar-tabs > ul > li');
     for (i = this._tabitems.length - 1; i >= 0; i--) {
         this._tabitems[i]._sidebar = this;
     }
 
-    // Find ol3-sb-sidebar > div.ol3-sb-sidebar-content > div.ol3-sb-sidebar-pane
+    // Find ol-sb-sidebar > div.ol-sb-sidebar-content > div.ol-sb-sidebar-pane
     this._panes = [];
     this._closeButtons = [];
     for (i = this._container.children.length - 1; i >= 0; i--) {
         child = this._container.children[i];
         if (child.tagName == 'DIV' &&
-                child.classList.contains('ol3-sb-sidebar-pane')) {
+                child.classList.contains('ol-sb-sidebar-pane')) {
             this._panes.push(child);
 
-            var closeButtons = child.querySelectorAll('.ol3-sb-sidebar-close');
+            var closeButtons = child.querySelectorAll('.ol-sb-sidebar-close');
             for (var j = 0, len = closeButtons.length; j < len; j++) {
                 this._closeButtons.push(closeButtons[j]);
             }
@@ -69,43 +69,43 @@ ol.control.Sidebar.prototype.setMap = function(map) {
 ol.control.Sidebar.prototype.open = function(id) {
     var i, child;
 
-    // hide old ol3-sb-active contents and show new content
+    // hide old ol-sb-active contents and show new content
     for (i = this._panes.length - 1; i >= 0; i--) {
         child = this._panes[i];
         if (child.id == id)
-            child.classList.add('ol3-sb-active');
-        else if (child.classList.contains('ol3-sb-active'))
-            child.classList.remove('ol3-sb-active');
+            child.classList.add('ol-sb-active');
+        else if (child.classList.contains('ol-sb-active'))
+            child.classList.remove('ol-sb-active');
     }
 
-    // remove old ol3-sb-active highlights and set new highlight
+    // remove old ol-sb-active highlights and set new highlight
     for (i = this._tabitems.length - 1; i >= 0; i--) {
         child = this._tabitems[i];
         if (child.querySelector('a').hash == '#' + id)
-            child.classList.add('ol3-sb-active');
-        else if (child.classList.contains('ol3-sb-active'))
-            child.classList.remove('ol3-sb-active');
+            child.classList.add('ol-sb-active');
+        else if (child.classList.contains('ol-sb-active'))
+            child.classList.remove('ol-sb-active');
     }
 
-    // open ol3-sb-sidebar (if necessary)
-    if (this.element.classList.contains('ol3-sb-collapsed')) {
-        this.element.classList.remove('ol3-sb-collapsed');
+    // open ol-sb-sidebar (if necessary)
+    if (this.element.classList.contains('ol-sb-collapsed')) {
+        this.element.classList.remove('ol-sb-collapsed');
     }
 
     return this;
 };
 
 ol.control.Sidebar.prototype.close = function() {
-    // remove old ol3-sb-active highlights
+    // remove old ol-sb-active highlights
     for (var i = this._tabitems.length - 1; i >= 0; i--) {
         var child = this._tabitems[i];
-        if (child.classList.contains('ol3-sb-active'))
-            child.classList.remove('ol3-sb-active');
+        if (child.classList.contains('ol-sb-active'))
+            child.classList.remove('ol-sb-active');
     }
 
-    // close ol3-sb-sidebar
-    if (!this.element.classList.contains('ol3-sb-collapsed')) {
-        this.element.classList.add('ol3-sb-collapsed');
+    // close ol-sb-sidebar
+    if (!this.element.classList.contains('ol-sb-collapsed')) {
+        this.element.classList.add('ol-sb-collapsed');
     }
 
     return this;
@@ -113,9 +113,9 @@ ol.control.Sidebar.prototype.close = function() {
 
 ol.control.Sidebar.prototype._onClick = function(evt) {
     evt.preventDefault();
-    if (this.classList.contains('ol3-sb-active')) {
+    if (this.classList.contains('ol-sb-active')) {
         this._sidebar.close();
-    } else if (!this.classList.contains('ol3-sb-disabled')) {
+    } else if (!this.classList.contains('ol-sb-disabled')) {
         this._sidebar.open(this.querySelector('a').hash.slice(1));
     }
 };
